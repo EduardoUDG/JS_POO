f88b779c9d6e323a6e04c7eef9558f05e64d4808
Eduardo Chavez PC <eduardo.cdorado@alumnos.udg.mx>
2021-06-21 16:19:01 -0500

    6

--- index.js
+++ index.js
@@ -15,8 +15,25 @@ class User {
         return this.name
     }
 };
+
+//* Herencia
+class Profesor extends User{
+    constructor(name, age, email, status, cursosDictados, calificacion) {
+        super(name, age, email, status)
+        this.cursosDictados = cursosDictados
+        this.calificacion = calificacion
+    }
+};
+
+class Alumno extends User{
+    constructor(name, age, email, status, cursosTomados) {
+        super(name, age, email, status, cursosTomados)
+        this.cursosTomados = cursosTomados
+    }
+};
  
 
-const person = new User('Lalo', 20, 'lalo@gmail.com', true)
-person.setName('Pepe')
-console.log(person.getName())
+const lalo = new Alumno('lalo', 20, 'lalo@gmail.com', true, ['HTML', 'CSS'])
+const samuel = new Profesor('Samuel', 48, 'samuel@gmail.com', true, ['Laravel', 'PHP'], 9)
+console.log(lalo)
+console.log(samuel.presentarse())
