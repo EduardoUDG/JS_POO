7d31038d738326dd585f598123e2bca6540467be
Eduardo Chavez PC <eduardo.cdorado@alumnos.udg.mx>
2021-06-21 15:37:29 -0500

    5

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const User = {
-    constructor(name, age, email, status){
+class User {
+    constructor(name, age, email, status) {
         this.name = name,
         this.age = age,
         this.email = email,
         this.status = status
     }
+    presentarse() {
+        return `Hi,  I'm ${this.name} y tengo ${this.age}`
+    }
+    setName(name) {
+        this.name = name
+    }
+    getName() {
+        return this.name
+    }
 };
  
 
-const person = new User('Lalo', 'lalo@gmail.com', 20, true)
-console.log(person) 
+const person = new User('Lalo', 20, 'lalo@gmail.com', true)
+person.setName('Pepe')
+console.log(person.getName())
