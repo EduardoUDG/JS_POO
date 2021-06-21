c995fccb86fb74968a64fa6cbd6dd25242e798c6
Eduardo Chavez PC <eduardo.cdorado@alumnos.udg.mx>
2021-06-21 12:16:32 -0500

    4

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const User = {
+    constructor(name, age, email, status){
+        this.name = name,
+        this.age = age,
+        this.email = email,
+        this.status = status
+    }
+};
+ 
 
-function User(name, age, email, status) {
-    this.name = name,
-    this.age = age,
-    this.email = email,
-    this.status = status
-}
-
-const person = new User('Lalo', 20, 'lalo@gmail.com', true)
+const person = new User('Lalo', 'lalo@gmail.com', 20, true)
 console.log(person) 
